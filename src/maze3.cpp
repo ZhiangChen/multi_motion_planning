19e5436605a8b59dc17a9393a7560e3a0725c998
ZhiangChen <zxc251@case.edu>
2016-05-01 00:09:08 -0400

    maze3

---
+++ src/maze3.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h> 
+#include <std_msgs/Float64.h> 
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/PointStamped.h>
+#include <iostream>
+#include <multi_motion_planning/rrt.h>
+
+using namespace std;
+
+
+
+int main(int argc, char **argv) 
+{ 
+  ros::init(argc,argv,"maze2"); 
+  ros::NodeHandle n; 
+  RRT Rrt(3,5);
+  Rrt.swapPositions();
+
+
+  return 0; 
+} 
